a680aebabbba416aee4718f02ebe414a312fb776
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-06-11 16:03:43 -0600

    basic strapi cms config

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f087f9e03424e403bc695517b06b9b52'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2b8b44d7-3777-43b5-90aa-5e0d1b90260d'
+};
